92013497151ba7b13e814995bb6f67158ec04c53
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2023-09-23 18:26:13 +0200

    fix(proxy): avoid breaking photo ID unique constraint when persisting
    When persisting photos, there is a chance that some of the photos
    returned by the Unsplash API exist already in the database.
    
    To avoid violating the ID uniqueness, the code needs to issue:
    * update statements for existing photos (to update their
      `last_used_date` and `json`),
    * insert statements for new photos.

--- packages/unsplash-proxy/src/storage/index.ts
+++ packages/unsplash-proxy/src/storage/index.ts
@@ -1,2 +1,4 @@
+// TODO: handle errors when executing database statements
+
 export { persistPhotos } from "./persist-photos";
 export { retrievePhotosToUse } from "./list-photos";
--- packages/unsplash-proxy/src/storage/persist-photos.ts
+++ packages/unsplash-proxy/src/storage/persist-photos.ts
@@ -6,22 +6,52 @@ export interface PhotoToPersist {
   lastUsedTimestamp: number;
 }
 
-export function persistPhotos({
+export async function persistPhotos({
   photosToPersist,
   database,
 }: {
   photosToPersist: PhotoToPersist[];
   database: D1Database;
 }): Promise<void> {
+  // NOTE: only insert photos that do not exist in the database already.
+  // Otherwise, the unique constraint is broken.
+  // For existing photos, only update them.
+  // Too bad there is no UPSERT.
+  const duplicatePhotoIDs = await database
+    .prepare(
+      `SELECT id FROM photos WHERE id IN (${Array.from({
+        length: photosToPersist.length,
+      })
+        .fill("?")
+        .join(", ")});`,
+    )
+    .bind(...photosToPersist.map(({ photo }) => photo.id))
+    .all<{ id: string }>();
+  const duplicatePhotoIDsSet = new Set(
+    duplicatePhotoIDs.results.map(({ id }) => id),
+  );
+
   const insertStatement = database.prepare(
     "INSERT INTO photos (id, json, last_used_date) VALUES (?1, ?2, ?3)",
   );
-
-  // TODO: handle errors when some photo was already added and there is a ID uniqueness violation
-  const photoInsertStatements = photosToPersist.map(
-    ({ photo, lastUsedTimestamp }) =>
-      insertStatement.bind(photo.id, serializePhoto(photo), lastUsedTimestamp),
+  const updateStatement = database.prepare(
+    "UPDATE photos SET last_used_date = ?, json = ? WHERE id = ?;",
   );
 
-  return database.batch(photoInsertStatements).then(() => undefined);
+  const photoInsertStatements = photosToPersist.map(
+    ({ photo, lastUsedTimestamp }) =>
+      duplicatePhotoIDsSet.has(photo.id)
+        ? updateStatement.bind(
+            lastUsedTimestamp,
+            serializePhoto(photo),
+            photo.id,
+          )
+        : insertStatement.bind(
+            photo.id,
+            serializePhoto(photo),
+            lastUsedTimestamp,
+          ),
+  );
+
+  await database.batch(photoInsertStatements);
 }
