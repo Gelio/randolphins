86e805e039c0da0d244277817296d60344957048
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2023-09-24 20:11:16 +0200

    refactor(slideshow): support aborting fetch requests for images
    Add support for `AbortSignal`.
    
    There is a separate variant of result returned when the request is
    aborted.

--- packages/slideshow/src/images/fetch.ts
+++ packages/slideshow/src/images/fetch.ts
@@ -5,6 +5,7 @@ export type FetchRandomDolphinUnsplashImagesError =
   | {
       type: "REACT_APP_UNSPLASH_PROXY_URL environment variable is not provided";
     }
+  | { type: "network error"; cause: Error }
   | { type: "response is not ok"; response: Response }
   | { type: "JSON deserialization failed"; cause: Error }
   | { type: "parsing photos failed"; cause: ZodError<UnsplashPhoto[]> };
@@ -14,15 +15,18 @@ type FetchRandomDolphinUnsplashImagesResult =
       variant: "error";
       cause: FetchRandomDolphinUnsplashImagesError;
     }
-  | { variant: "success"; photos: UnsplashPhoto[] };
+  | { variant: "success"; photos: UnsplashPhoto[] }
+  | { variant: "aborted" };
 
 /**
  * Fetches random dolphin photos from Unsplash, using the unsplash-proxy.
  */
 export async function fetchRandomDolphinUnsplashImages({
   count,
+  abortSignal,
 }: {
   count: number;
+  abortSignal?: AbortSignal;
 }): Promise<FetchRandomDolphinUnsplashImagesResult> {
   if (!process.env.REACT_APP_UNSPLASH_PROXY_URL) {
     return {
@@ -36,11 +40,29 @@ export async function fetchRandomDolphinUnsplashImages({
   const unsplashProxyURL = new URL(process.env.REACT_APP_UNSPLASH_PROXY_URL);
   unsplashProxyURL.searchParams.set("count", count.toString());
 
-  const response = await fetch(unsplashProxyURL, {
-    headers: {
-      Accept: "application/json",
-    },
-  });
+  let response: Response;
+  try {
+    response = await fetch(unsplashProxyURL, {
+      headers: {
+        Accept: "application/json",
+      },
+      signal: abortSignal ?? null,
+    });
+  } catch (error) {
+    if (error instanceof Error && error.name === "AbortError") {
+      return {
+        variant: "aborted",
+      };
+    } else {
+      return {
+        variant: "error",
+        cause: {
+          type: "network error",
+          cause: error as Error,
+        },
+      };
+    }
+  }
 
   if (!response.ok) {
     return {
